11bcccbc2f9e353511120771e1e4830661fdffb0
Myles Borins <myles.borins@gmail.com>
2012-06-30 01:51:59 -0400

    Update version number

--- libmonome.rb
+++ libmonome.rb
@@ -4,7 +4,7 @@ class Libmonome < Formula
   head 'https://github.com/monome/libmonome.git'
   homepage 'http://monome.org'
   md5 'bf2a21ef4ccc361a6b33f099f9fade12'
-  version '1.0'
+  version '1.1'
   depends_on 'liblo'
 
   def install
