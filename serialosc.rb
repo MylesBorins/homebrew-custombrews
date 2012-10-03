10d21f020d0f612dba4b4e23d9f6072b98138728
Myles Borins <myles.borins@gmail.com>
2012-10-02 20:43:36 -0700

    Update serialosc

--- serialosc.rb
+++ serialosc.rb
@@ -1,9 +1,12 @@
 require 'formula'
 
 class Serialosc < Formula
+  homepage 'http://docs.monome.org/doku.php?id=app:serialosc'
+  url 'https://github.com/monome/serialosc/tarball/1.2a'
+  sha1 '82cbc0aa09367dee79a16d1163e35d7c7f47bec3'
   head 'https://github.com/monome/serialosc.git'
-  homepage 'http://www.monome.org'
-  version '1.0'
+  version '1.2a'
+
   depends_on 'liblo'
   depends_on 'confuse'
   depends_on 'libmonome'
