e11229032d43344633c6563cec7ca91fb0715ffd
Myles Borins <myles.borins@gmail.com>
2013-01-09 01:14:27 -0800

    Added enable-debug option

--- stk.rb
+++ stk.rb
@@ -4,15 +4,27 @@ class Stk < Formula
   homepage 'https://ccrma.stanford.edu/software/stk/'
   url 'http://ccrma.stanford.edu/software/stk/release/stk-4.4.4.tar.gz'
   sha1 '2a94f02ed054d3b991352cc68a85a0a8063e3a4b'
+  
+  depends_on :automake
+
+  option "enable-debug", "Compile with debug flags and modified CFLAGS for easier debugging"
 
   def install
-    # inreplace 'configure', 'CFLAGS="-g -O2"', 'CFLAGS="-g -O0"'
-    # inreplace 'configure', 'CFLAGS="-g"', 'CFLAGS="-g -O0"'
-    ENV['CFLAGS'] = "-g -O0"
-    system "./configure", "--enable-debug", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}"
 
-    print("test")
+    if build.include? "enable-debug"
+      inreplace 'configure', 'CFLAGS="-g -O2"', 'CFLAGS="-g -O0"'
+      inreplace 'configure', 'CXXFLAGS="-g -O2"', 'CXXFLAGS="-g -O0"'
+      debug_status = "--enable-debug"
+    else
+      debug_status = "--disable-debug"
+    end
+    
+    
+
+    system "./configure", "--prefix=#{prefix}",
+                          debug_status,
+                          "--disable-dependency-tracking"
+
     system "make"
 
     lib.install 'src/libstk.a'
